a3d2a4593e44b8601343a6aea580a14d4e6135cc
Amy Plant <amyplant@me.com>
2020-02-01 20:35:55 -0600

    fix | don't show Authors if there aren't any

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,9 +19,12 @@ class Book extends Component {
                     updateBookShelf={updateBookShelf} 
                 />
                 <div className="book-title">{book.title}</div>
-                <div className="book-authors">
-                    {book.authors.join(', ')}
-                </div>
+
+                {book.authors &&
+                    <div className="book-authors">
+                        {book.authors.join(', ')}
+                    </div>
+                }
             </div>
         )
     }
